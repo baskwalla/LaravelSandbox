91f9df62e2b7e1b33c81e24bfb7dc8f5125703c4
Chad <baskwalla@gmail.com>
2020-11-29 12:33:08 -0600

    first pass at views

---
+++ resources/views/image-upload.blade.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Image Upload</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    Image Upload
+                </div>
+
+                <form action="{{ route('uploadimage') }}" enctype="multipart/form-data" method="post">
+                @csrf
+                    <div>
+                        <label for="image">Image Upload</label>
+                        <input type="file"
+                            id="image" name="image"
+                            accept="image/png, image/jpeg, image/jpg">
+                    </div>
+                    <div>
+                        <button class="btn btn-primary">Upload</button>
+                    </div>
+                </form>
+                @if (session('success'))
+                    <div class="alert alert-success alert-dismissable">
+                        <button type="button" class="close" data-dismiss="alert">&times;</button>
+                        {{session('success')}}
+                    </div>
+                @endif
+            </div>
+        </div>
+    </body>
+</html>
